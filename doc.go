a1ea9c47a3c8a17a94d5fa6dd6562dd0710886cf
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2013-09-30 19:22:57 -0700

    As a convention, doc.go contains package comment, introductory documentation, and package clause

---
+++ doc.go
@@ -0,0 +1,8 @@
+/*
+Grohl is an opinionated library for outputting logs in a key=value structure.
+This event data is used to drive metrics and monitoring services.
+
+Dave Grohl is the lead singer of Foo Fighters.  I hear he's also passionate
+about event driven metrics.
+*/
+package grohl
